9def10aaf150402b81d6eab8f57d3406905bd542
Gabriel Henrique da Silva Barbosa <gabrielrick1990@gmail.com>
2021-08-31 16:41:26 -0300

    update style

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ function verificar(){
 	var res = document.getElementById('res')
 	var md = (n1 + n2 + n3)/3;
 	if (md >=7 && md < 10){
-		res.innerHTML = `Sua média foi ${md} por isso você passou!`
-		document.body.style.background ='#0be881'
+		res.innerHTML = `Sua média foi ${md} por isso você passou, parabéns!`
+		document.body.style.background ='#05c46b'
 	}	
 		else if(md <= 6 && md > 1){
-		res.innerHTML = `Sua média foi ${md} por isso você foi reprovado! se esfore mais ano que vem`
-		document.body.style.background ='#ff3f34'
+		res.innerHTML = `Sua média foi ${md} por isso você foi reprovado! se esforce mais ano que vem`
+		document.body.style.background ='#d63031'
 	}
 	else if(md == 10){
 		res.innerHTML = `Sua média foi ${md} por isso você passou, parabéns pelo 10 você mandou bem!`
-		document.body.style.background ='#34e7e4'
+		document.body.style.background ='#7bed9f'
 	}else if(md == 0){
 		res.innerHTML = `Sua média foi ${md} por isso você reprovou!,você foi muito mal se esforce mais ano que vem! `
-		document.body.style.background ='#1e272e'
+		document.body.style.background ='#2d3436'
 			
 
 		}else{
 		res.innerHTML =`Sua media foi ${md} o que não é uma nota valida tente novamente com os valores certos!(de 1 a 10)`
-		document.body.style.background ='#3c40c6'
+		document.body.style.background ='#ffeaa7'
 	}
 }
