612af9a12294e4fe419efc5ff5fd9a0b89eb24b3
Gabriel Henrique <gabrielrick1990@gmail.com>
2021-09-27 20:39:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,20 @@ function verificar(){
 		res.innerHTML = `Sua média foi ${md.toFixed(2)} por isso você passou, parabéns!`
 		document.body.style.background ='#05c46b'
 	}
-		else if(md <= 6 && md > 0){
-		res.innerHTML = `Sua média foi ${md.toFixed(2)} por isso você foi reprovado! se esforce mais ano que vem`
-		document.body.style.background ='#d63031'
+		else if(md >= 5 && md < 7){
+		res.innerHTML = `Sua média foi ${md.toFixed(2)} por isso você foi aprovado! mas se esforce mais ano que vem`
+		document.body.style.background ='#ff7f50'
 	}
+		else if(md < 5){
+		res.innerHTML = `Sua média foi ${md.toFixed(2)} por isso você está de recuperação`
+		document.body.style.background ='#ff4757'
+		}
 	else if(md == 10){
 		res.innerHTML = `Sua média foi ${md.toFixed(2)} por isso você passou, parabéns pelo 10 você mandou bem!`
 		document.body.style.background ='#7bed9f'
 	}else if(md == 0){
 		res.innerHTML = `Sua média foi ${md.toFixed(2)} por isso você reprovou!,você foi muito mal se esforce mais ano que vem! `
 		document.body.style.background ='#2d3436'
-
-
 		}else if(md > 10){
 		res.innerHTML =`Sua media foi ${md.toFixed(2)} o que não é uma nota valida tente novamente com os valores certos!(de 1 a 10)`
 		document.body.style.background ='#F8EFBA'
